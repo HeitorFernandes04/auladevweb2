148ae61f8446dcd5c97244dc5d591b181c2d0cf1
HeitorFernandes04 <heitorfernandescarrijo@gmail.com>
2026-04-01 21:22:41 -0300

    aula 01/04

---
+++ sistema/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+class Login (View):
+
+    def get(self, request):
+        contexto = {}
+        return render (request, 'autenticacao.html', contexto)
+    
